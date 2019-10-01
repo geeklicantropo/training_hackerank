03a6834283041b90b31721c87cce24eb1a53e472
Wagner Lobo <wagner.lobobr@gmail.com>
2019-10-01 01:53:26 -0300

    Update triplets.py
    Final answer

--- hackerank/triplets.py
+++ hackerank/triplets.py
@@ -6,6 +6,20 @@ import sys
 
 # Complete the compareTriplets function below.
 def compareTriplets(a, b):
+    alice = 0
+    bob = 0
+    for i in range(3):
+        if(a[i] > 100 or b[i] > 100 or a[i] < 1 or b[i] < 1):
+            print("Value must be between 1 and 100")
+            continue
+        elif (a[i] > b[i]):
+            alice += 1
+        elif (a[i] < b[i]):
+            bob += 1
+        else:
+            alice +=0
+            bob +=0
+    return (alice, bob)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
